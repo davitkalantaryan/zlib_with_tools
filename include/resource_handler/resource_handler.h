969bd29bcc129556570e029b2a31e8b5ae8b4f26
DAVIT Kalantaryan <Davit Kalantaryan>
2023-02-19 15:12:15 +0100

    Adde API to set icon for application

---
+++ include/resource_handler/resource_handler.h
@@ -0,0 +1,23 @@
+//
+// file:			resource_handler.h
+// path:			include/resource_handler/resource_handler.h
+// created on:		2023 Feb 19
+// created by:		Davit Kalantaryan (davit.kalantaryan@desy.de)
+//
+
+#ifndef ZLIB_WITH_TOOLS_INCLUDE_RESOURCE_HANDLER_RESOURCE_HANDLER_H
+#define ZLIB_WITH_TOOLS_INCLUDE_RESOURCE_HANDLER_RESOURCE_HANDLER_H
+
+#include <zlib_with_tools/export_symbols.h>
+
+
+CPPUTILS_BEGIN_C
+
+
+ZLIBANDTLS_EXPORT int ResourceHandlerSecIconToApp(const char* a_path_to_app, const char* a_path_to_icon);
+
+
+CPPUTILS_END_C
+
+
+#endif  // #ifndef ZLIB_WITH_TOOLS_INCLUDE_RESOURCE_HANDLER_RESOURCE_HANDLER_H
---
+++ src/tests/main_change_or_add_icon_test.cpp
@@ -0,0 +1,44 @@
+//
+// file:			main_hello_world_test.cpp
+// path:			src/tests/main_hello_world_test.cpp
+// created on:		2023 Feb 17
+// created by:		Davit Kalantaryan (davit.kalantaryan@desy.de)
+//
+
+#include <zlib_with_tools/export_symbols.h>
+#include <zlib_with_tools/utils/stdio_zlibandtls.h>
+#include <zlib_with_tools/utils/memory_zlibandtls.h>
+#include <resource_handler/resource_handler.h>
+#include <iostream>
+#include <string.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#define ZLIBWT_EXE_NM_TO_SET_ICON               "hello_world_test.exe"
+#define ZLIBWT_EXE_NM_TO_SET_ICON_LEN_PLUS1     21
+
+
+int main(int a_argc, char* a_argv[])
+{
+
+    // icon = ExtractIconA(0, a_argv[1], 0);
+    size_t unBeginSizePlus1;
+    char* pcBuffer;
+    char* pcTerm = strrchr(a_argv[0], '\\');
+    assert(pcTerm);
+
+    if (a_argc < 2) {
+        ::std::cerr << "icon file is not specified\n";
+        return 1;
+    }
+
+    unBeginSizePlus1 = ((size_t)(pcTerm - a_argv[0]))+1;
+    pcBuffer = (char*)alloca_zlibandtls(unBeginSizePlus1 + 4 + ZLIBWT_EXE_NM_TO_SET_ICON_LEN_PLUS1);
+    memcpy(pcBuffer, a_argv[0], unBeginSizePlus1);
+    memcpy(pcBuffer + unBeginSizePlus1, ZLIBWT_EXE_NM_TO_SET_ICON, ZLIBWT_EXE_NM_TO_SET_ICON_LEN_PLUS1);
+
+
+    return ResourceHandlerSecIconToApp(pcBuffer,a_argv[1]);
+}
