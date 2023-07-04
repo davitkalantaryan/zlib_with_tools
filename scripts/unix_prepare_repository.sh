2e5a73df565cdeff7f14b8bceb3af37bb4609a12
Davit Kalantaryan <davit.kalantaryan@desy.de>
2023-07-04 10:11:45 +0200

    adedd script scripts/unix_prepare_repository.sh

---
+++ scripts/unix_prepare_repository.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# https://intoli.com/blog/exit-on-errors-in-bash-scripts/
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+#trap 'echo "\"${last_command}\" command finished with exit code $?."' EXIT
+
+
+# thanks to https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux
+if [[ "$(uname)" == "Darwin" ]]; then
+	# Do something under Mac OS X platform
+	lsbCode=mac
+elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
+	# Do something under GNU/Linux platform
+	lsbCode=`lsb_release -sc`
+#elif [[ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]]; then
+#	# Do something under 32 bits Windows NT platform
+#elif [[ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]]; then
+#	# Do something under 64 bits Windows NT platform
+#else
+fi
+echo "lsbCode="$lsbCode
+
+#scriptFileFullPath=`readlink -f ${0}`
+#scriptDirectory=`dirname ${scriptFileFullPath}`
+#cd ${scriptDirectory}/../..
+#repositoryRoot=`pwd`
+
+# in mac os above short calculation will not work
+# also `realpath` utilit is missing in mac
+
+scriptDirectory=`dirname "${0}"`
+scriptFileName=`basename "${0}"`
+cd "${scriptDirectory}"
+fileOrigin=`readlink "${scriptFileName}"` || :
+while [ ! -z "${fileOrigin}" ]
+do
+	scriptDirectory=`dirname "${fileOrigin}"`
+	scriptFileName=`basename "${fileOrigin}"`
+	cd "${scriptDirectory}"
+	fileOrigin=`readlink "${scriptFileName}"`  || :
+done
+cd ..
+repositoryRoot=`pwd`
+echo repositoryRoot=$repositoryRoot
+
+
+git submodule sync --recursive
+git submodule update --init --recursive
+
+#${repositoryRoot}/scripts/part_debian_per_host_using_apt.sh
+echo "!!!!!!!!!!!!!!!!!!! Run ${repositoryRoot}/scripts/part_debian_per_host_using_apt.sh"
+
+${repositoryRoot}/contrib/qtutils/scripts/.cicd/unix_prebuild.sh
